9647ba27a70b0cde42561b3ffce9a6a286f16409
Brian Wiebe <51461287+wiebecoding@users.noreply.github.com>
2022-08-19 20:40:14 -0700

    Added Hours to Route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route, Routes, Link} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 import EditItems from "./components/edit-items.component";
 import EditHours from "./components/edit-hours.component";
@@ -10,38 +10,12 @@ import Website from "./components/website.component";
 function App() {
   return (
     <Router>
-      <div>
-        <ul>
-            <li>
-              <Link to="/"></Link>
-            </li>
-            <li>
-              <Link to="/menu-edit/:id">Edit Item</Link>
-            </li>
-            <li>
-              <Link to="/menu-add">Add Menu Item</Link>
-            </li>
-            <li>
-              <Link to="/hours-edit">Change Hours</Link>
-            </li>
-          </ul>
-
-        <hr />
-        <Routes>
-          <Route exact path='/'>
-            <Website/>
-          </Route>
-          <Route path="/menu-edit/:id">
-            <EditItems/>
-          </Route>
-          <Route path="/menu-add">
-            <AddItems/>
-          </Route>
-          <Route path="/hours-edit">
-            <EditHours/>
-          </Route>
-        </Routes>
-      </div>
+      <Routes>
+        <Route path='/' exact element={<Website />} />
+        <Route path='/menu-edit/:id' element={<EditItems/>} />
+        <Route path='/menu-add' element={<AddItems/>} />
+        <Route path="/add-hours" element={<EditHours/>} />
+      </Routes>
     </Router>
   );
 }
